bd4848974252cebc85549541a7eff3a34b604f2c
Murilo Boratto <murilo.boratto@fieb.org.br>
2022-05-17 16:48:09 -0300

    Update module load openmpi-cuda

--- samples/ping-pong/MPI+CUDA/slurm-MPI-CUDA.sh
+++ samples/ping-pong/MPI+CUDA/slurm-MPI-CUDA.sh
@@ -7,6 +7,6 @@
 #SBATCH --ntasks-per-node=1                    # 1 job per node
 #SBATCH --account=cenpes-lde                   # Account of the group 
 
-module load openmpi/4.1.1-cuda
+module load openmpi/4.1.1-cuda-11.6-ofed-5.4
 
 mpirun -np 2 -x UCX_MEMTYPE_CACHE=n  -mca pml ucx -mca btl ^vader,tcp,openib,smcuda -x UCX_NET_DEVICES=mlx5_0:1  ./ping-pong-MPI-CUDA
